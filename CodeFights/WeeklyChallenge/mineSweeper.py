1400fa06f164092b07500b5e82f2db306f80d19f
finite <finite.impulse@gmail.com>
2017-12-18 07:17:55 +0100

    mineSweeper

--- CodeFights/WeeklyChallenge/mineSweeper.py
+++ CodeFights/WeeklyChallenge/mineSweeper.py
@@ -9,19 +9,19 @@
 import copy
 
 def minesweeper(matrix):
-    new_m = copy.deepcopy(matrix[:])
-    ln_r = len(matrix)
-    ln_c = len(matrix[0])
-    for r in range(ln_r):
-        for c in range(ln_c):
+    m = copy.deepcopy(matrix[:])
+    row = len(matrix)
+    col = len(matrix[0])
+    for r in range(row):
+        for c in range(col):
             b = 0
             for x in range(r - 1, r + 2):
                 for y in range(c - 1, c + 2):
-                    if 0 <= x < ln_r and 0 <= y < ln_c:
+                    if 0 <= x < row and 0 <= y < col:
                         if matrix[x][y] == True:
                             b += 1
                 if matrix[r][c] == True:
-                    new_m[r][c] = b - 1
+                    m[r][c] = b - 1
                 else:
-                    new_m[r][c] = b
-    return new_m
+                    m[r][c] = b
+    return m
