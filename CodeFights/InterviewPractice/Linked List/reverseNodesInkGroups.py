97338db630dce081fd735c23d5f0582551e3741a
finite <finite.impulse@gmail.com>
2018-01-24 03:56:12 +0100

    ReverseNodesInKGroups update

--- CodeFights/InterviewPractice/Linked List/reverseNodesInkGroups.py
+++ CodeFights/InterviewPractice/Linked List/reverseNodesInkGroups.py
@@ -7,18 +7,18 @@
 # You may not alter the values in the nodes - only the nodes themselves can be changed.
 
 # clues
-#
+
 # The basic steps are:
-#
+
 # 0. Start at head node, set to current;
-#
+
 # 1. Scan ahead K elements from current position with a separate pointer, ahead.
 # If you reach the end, just return, because you do not have to reverse 'partial' groups.
-#
+
 # 2. Reverse the list between your current pointer and the pointer that is K away.
 # The only difference from a 'normal' reversing of a linked list is that you need to pay attention to what ahead.next originally pointed to.
 # You cannot just assume it was NULL.
-#
+
 # 3. Set current to the original ahead.next node, and go back to step 1
 
 class ListNode(object):
@@ -32,6 +32,12 @@ def reverseNodesInKGroups(l, k):
     head_prev = None
     head_next = None
     step = k
+    cycle = size(l)//k
+    if cycle < 1:
+        head_next = current
+        while current != None:
+            current = current.next
+        return head_next
     while current != None and step > 0:
         head_next = current.next
         current.next = head_prev
@@ -41,3 +47,11 @@ def reverseNodesInKGroups(l, k):
     if head_next != None:
         l.next = reverseNodesInKGroups(head_next, k)
     return head_prev
+
+def size(l):
+    size = 0
+    current = l
+    while current != None:
+        size += 1
+        current = current.next
+    return size
