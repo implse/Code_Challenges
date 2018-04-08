11ad8140fba2f34996fc7fdf98529d9c0afea84a
finite <finite.impulse@gmail.com>
2018-04-08 03:55:05 +0200

    Add Huffman Decoding

--- HackerRank/Tree/Tree_HuffmanDecoding.py
+++ HackerRank/Tree/Tree_HuffmanDecoding.py
@@ -4,7 +4,7 @@
 # More frequent characters are assigned shorter codewords and less frequent characters are assigned longer codewords.
 # A Huffman tree is made for the input string and characters are decoded based on their position in the tree.
 # We add a '0' to the codeword when we move left in the binary tree and a '1' when we move right in the binary tree.
-jj
+
 class Node:
     def __init__(self, freq,data):
         self.freq= freq
