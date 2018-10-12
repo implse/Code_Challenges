c42fe5dfe29a7a63e57d410c4c8c0b5b1380ae65
Finite <finite.impulse@gmail.com>
2018-10-12 02:19:49 +0200

    Backtracking

---
+++ CodeFights/InterviewPractice/Backtracking/climbingStaircase.py
@@ -0,0 +1,5 @@
+# https://app.codesignal.com/interview-practice/task/cAXEnPyzknC5zgd7x
+ 
+# You need to climb a staircase that has n steps, and you decide to get some extra exercise by jumping up the steps.
+# You can cover at most k steps in a single jump.
+# Return all the possible sequences of jumps that you could take to climb the staircase, sorted.
