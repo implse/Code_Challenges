e84b3bae40ca33281221444aba904f1c07220690
Finite <finite.impulse@gmail.com>
2018-12-05 19:17:04 +0100

    Add Linked List swap every 2 nodes

---
+++ Random/LinkedList_swap.py
@@ -0,0 +1,16 @@
+# Given the head of a singly linked list, swap every two nodes and return its head.
+
+# For example, given 1 -> 2 -> 3 -> 4, return 2 -> 1 -> 4 -> 3.
+
+class Node(object):
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+def swap_by_two(node):
+    while node:
+        temp = node.value
+        node.value = node.next.value
+        node.next.value = temp
+        node = node.next.next
+    return node
