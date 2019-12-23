8c293aeff8aa0681e27bc6a5f8728149407397bb
Finite <finite.impulse@gmail.com>
2019-12-23 04:38:00 +0100

    SpaceX packetDescrambler

--- SpaceX/packetDescrambler.py
+++ SpaceX/packetDescrambler.py
@@ -1,160 +1,39 @@
 def packetDescrambler(seq, fragmentData, n):
-
-    # Construct hash table seq/fragment
-    sequence = {}
+    # build sequence_ids dict
+    sequence_ids = {}
     for i in range(len(seq)):
         id = seq[i]
         fragment = fragmentData[i]
-        if id not in sequence:
-            sequence[id] = [fragment]
+        if id not in sequence_ids.keys():
+            sequence_ids[id] = [fragment]
         else:
-            sequence[id].append(fragment)
-
-    ids = list(sequence.keys())
-    ids.sort()
+            sequence_ids[id].append(fragment)
+    ids = sorted(list(sequence_ids.keys()))
     message = ""
 
     for i in range(len(ids)):
+        # test increase id order 0, 1, 2 ...
         if i != ids[i]:
             return ""
-        fragment = validate_fragment(sequence[ids[i]], n*0.5)
-        if fragment != None:
-            message += fragment
-    return message if len(message) >= n and message[-1] == "#" or message == "#" else ""
+        # test fragment
+        fragment = validate_fragment(sequence_ids[i], n)
+        if fragment == False:
+            return ""
+        message += fragment
+    return validate_message(message)
 
-def validate_fragment(lst, threshold):
+def validate_fragment(lst, n):
+    threshold = n * 0.5
     for fragment in set(lst):
-        if lst.count(fragment) >= threshold:
+        if lst.count(fragment) > threshold:
             return fragment
-    return None
+    return False
 
+def validate_message(message):
+    return message if message[-1] == "#" and message.count('#') == 1 else ""
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-# Old
-from math import ceil
-def packetDescrambler(seq, fragmentData, n):
-    # n : number of copies of each fragment (50% of n to validate)
-    # seq : number or id of the fragment
-    # fragmentData : single character from the message
-    # return : reconstruct message or empty string
-
-    # rules :
-        #1 50% of n
-        #2 last fragment "#"
-        #3 no gap
-
-    # Validate sequence (increasing order)
-    ids = list(set(id for id in seq))
-    if validate_seq(ids) == False:
-        return ""
-
-    # Construct hash table seq/fragment
-    sequence = {}
-    for i in range(len(seq)):
-        id = seq[i]
-        if id not in sequence:
-            sequence[id] = [fragmentData[i]]
-        else:
-            sequence[id].append(fragmentData[i])
-
-    message = ""
-    threshold = math.ceil(n*0.5)
-    for id in ids:
-        validate_id = validate_fragment(sequence[id], threshold)
-        #print(validate_id)
-        if validate_id != None:
-            message += validate_id
-    return message if len(message) >= n and message[-1] == "#" or message == "#" else ""
-
-def validate_fragment(lst, threshold):
-    fragment = ""
-    count = 0
-    # searching for the most frequent fragment
-    # and check if 50% of n to be validate
-    for value in lst:
-        compare = max(count, lst.count(value))
-        if compare  > count:
-            count = compare
-            fragment = value
-    if count >= threshold:
-        return fragment
-    return None
-
-def validate_seq(ids):
-    for i in range(1, len(ids)):
-        if ids[i-1] != ids[i]-1:
-            return False
-    return True
-
-# last
-
-from math import ceil
-def packetDescrambler(seq, fragmentData, n):
-    # n : number of copies of each fragment (50% of n to validate)
-    # seq : number or id of the fragment
-    # fragmentData : single character from the message
-    # return : reconstruct message or empty string
-
-    # rules :
-        #1 50% of n
-        #2 last fragment "#"
-        #3 no gap
-
-    # Validate sequence (increasing order)
-    ids = set(seq)
-    if validate_seq(ids) == False:
-        return ""
-
-    # Construct hash table seq/fragment
-    sequence = {}
-    for i in range(len(seq)):
-        id = seq[i]
-        fragment = fragmentData[i]
-        if id not in sequence:
-            sequence[id] = [fragment]
-        else:
-            sequence[id].append(fragment)
-
-    message = ""
-    threshold = math.ceil(n*0.5)
-    for id in ids:
-        validate_id = validate_fragment(sequence[id], threshold)
-        #print(validate_id)
-        if validate_id != None:
-            message += validate_id
-    return message if len(message) >= n and message[-1] == "#" or message == "#" else ""
-
-def validate_fragment(lst, threshold):
-    fragment = ""
-    count = 0
-    set_lst = set(lst)
-    # searching for the most frequent fragment
-    # and check if 50% of n to be validate
-    #for value in lst:
-    for value in set_lst:
-        compare = max(count, lst.count(value))
-        if compare  > count:
-            count = compare
-            fragment = value
-    if count >= threshold:
-        return fragment
-    return None
-
-def validate_seq(ids):
-    ids = list(ids)
-    for i in range(1, len(ids)):
-        if ids[i-1] != ids[i]-1:
-            return False
-    return True
+# Test
+seq = [1, 1, 0, 0, 0, 2, 2, 2]
+fragments = ['+', '+', 'A', 'A', 'B', '#', '#', '#']
+n = 3
+print(packetDescrambler(seq, fragments, n))
