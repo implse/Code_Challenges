9fe2608b98a1fea175e52267ed74b252cc76bf50
Finite <finite.impulse@gmail.com>
2020-02-05 11:22:05 +0100

    Update launchSequenceChecker

--- SpaceX/launchSequenceChecker.py
+++ SpaceX/launchSequenceChecker.py
@@ -2,9 +2,7 @@
 
 def launchSequenceChecker(systemNames, stepNumbers):
     sequence = dict()
-    for i in range(len(systemNames)):
-        systemId = systemNames[i]
-        stepNumber = stepNumbers[i]
+    for systemId, stepNumber in zip(systemNames, stepNumbers):
         if systemId not in sequence:
             sequence[systemId] = [stepNumber]
         elif stepNumber <= sequence[systemId][-1]:
